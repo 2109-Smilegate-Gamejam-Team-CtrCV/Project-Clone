70aa2353df05dfeb3a05e9faad2d39c0440f7b33
qwo330 <qwd330@gmail.com>
2021-09-25 15:47:43 +0900

    1. 클론 애니메이션 적용 2. 클론, 몬스터 충돌 시 반동 추가

--- ProjectClone/Assets/_Scripts/Actor/Actor.cs
+++ ProjectClone/Assets/_Scripts/Actor/Actor.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class Actor : MonoBehaviour
+public class Actor : MonoBehaviour, IReboundable
 {
-    [HideInInspector] public int HP;
+    protected string[] EnemyTags; // 피격 판정 있는 태그
+
+    public int HP;
     public int maxHP = 6;
 
     public float moveSpeed = 1f;
+    public float reboundPower = 1f;
+
+    protected SpriteRenderer render;
+    protected Rigidbody2D rb;
+    protected Animator animator;
+
+    protected virtual void Awake()
+    {
+        render = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        Init();
+    }
 
     protected virtual void Update()
     {
@@ -33,6 +50,34 @@ public class Actor : MonoBehaviour
     }
 
     public virtual void Dead() { }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ContainEnemyTag(collision.collider.tag))
+        {
+            Vector2 reboundDir = (transform.position2D() - collision.transform.position2D()).normalized;
+            Rebound(reboundDir);
+        }
+    }
+
+    protected bool ContainEnemyTag(string tag)
+    {
+        if (EnemyTags == null || EnemyTags.Length < 1)
+            return false;
+
+        return EnemyTags.Contains(tag);
+    }
+
+    public void Rebound(Vector2 direction)
+    {
+        // todo : 충돌 시, 반대 방향으로 튕겨나간다.
+        rb.AddForce(direction * reboundPower, ForceMode2D.Impulse);
+    }
+}
+
+public interface IReboundable
+{
+    public void Rebound(Vector2 direction);
 }
 
 public interface IAttackabale
